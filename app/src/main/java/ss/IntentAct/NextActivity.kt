1e36c861ca3a70ff6022f2e07d74c5883c3ca170
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-06-15 16:35:32 +0530

    Update NextActivity.kt

--- app/src/main/java/ss/IntentAct/NextActivity.kt
+++ app/src/main/java/ss/IntentAct/NextActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
