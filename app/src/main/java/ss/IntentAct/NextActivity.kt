3c9f10be4a93fab4b5c3667d1133e0ad92de22fa
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-06-15 16:46:42 +0530

    Update NextActivity.kt

--- app/src/main/java/ss/IntentAct/NextActivity.kt
+++ app/src/main/java/ss/IntentAct/NextActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Scaffold
@@ -22,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 //import androidx.compose.ui.tooling.preview.Preview
 
 import ss.IntentAct.ui.theme.IntentActTheme
